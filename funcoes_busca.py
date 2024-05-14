0a40daed1c893c7e1dc9be248c8ac8ab956e7943
Lucas Rodegheri <137840882+RodegheriLucas@users.noreply.github.com>
2024-05-13 22:21:11 -0300

    Ajustes pendentes

--- funcoes_busca.py
+++ funcoes_busca.py
@@ -1,10 +1,16 @@
+import time
+
 def linesrch(list, value):
+    inicio = time.time()
     for i in range(len(list)):
         if list[i] == value:
-            return f'O valor {value} foi encontrado na posição {i} da lista'
+            fim = time.time()
+            cronometro = fim - inicio
+            return f'O valor {value} foi encontrado na posição {i} da lista em {cronometro:.2f} segundos'
     return f'O valor {value} não foi encontrado na lista'
 
 def binarysrch(list, value):
+    inicio = time.time()
     left = 0
     right = len(list) - 1
     
@@ -12,7 +18,9 @@ def binarysrch(list, value):
         mid = (left+right)//2
         
         if list[mid]==value:
-            return f'O valor {value} foi encontrado na posição {mid}'
+            fim = time.time()
+            cronometro = fim - inicio
+            return f'O valor {value} foi encontrado na posição {mid} em {cronometro:.2f} segundos'
         
         elif list[mid]<value:
             left = mid + 1
